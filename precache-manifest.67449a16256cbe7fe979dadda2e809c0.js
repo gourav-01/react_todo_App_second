521488c028478853758ef37da10f26327caefc52
Gourav <gourav.gks01@gmail.com>
2021-11-18 18:11:02 +0530

    Updates

---
+++ precache-manifest.67449a16256cbe7fe979dadda2e809c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57497dbb37870893e3b807f1ca8fa6aa",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "56464aea55d642340e49",
+    "url": "/react-todo-list/static/css/main.b1612724.chunk.css"
+  },
+  {
+    "revision": "3901205ca6f8dd05d65e",
+    "url": "/react-todo-list/static/js/2.7ab3eef2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.7ab3eef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56464aea55d642340e49",
+    "url": "/react-todo-list/static/js/main.561009be.chunk.js"
+  },
+  {
+    "revision": "91a55604ac59526b5881",
+    "url": "/react-todo-list/static/js/runtime-main.a072b4a7.js"
+  },
+  {
+    "revision": "465dbcf201aa73783679591c942ef95f",
+    "url": "/react-todo-list/static/media/trash.465dbcf2.svg"
+  }
+]);
